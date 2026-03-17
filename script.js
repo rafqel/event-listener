4f212c3de46911fb454ead6b4ae4c5f40f5903a9
Rafael Alves <rafqel.alves@hotmail.com>
2026-03-17 12:57:06 -0300

    firstCommit

---
+++ script.js
@@ -0,0 +1,17 @@
+const myBox = document.getElementById("myBox");
+const myButton = document.getElementById("myButton");
+
+myButton.addEventListener("click", (event) => {
+  myBox.style.backgroundColor = "tomato";
+  myBox.textContent = "OUCH! 🤕";
+});
+
+myButton.addEventListener("mouseover", (event) => {
+  myBox.style.backgroundColor = "yellow";
+  myBox.textContent = "Don't do it 😲";
+});
+
+myButton.addEventListener("mouseout", (event) => {
+  myBox.style.backgroundColor = "lightgreen";
+  myBox.textContent = "Click Me 😀";
+});
